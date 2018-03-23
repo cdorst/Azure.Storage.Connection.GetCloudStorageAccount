57da665d1eccae0f804f12152915f2e8f8cd4075
Christopher Dorst <chris@dorst.email>
2018-03-23 12:32:55 -0500

    Update CloudStorageAccountGetter.cs

--- Azure.Storage.Connection.GetCloudStorageAccount/CloudStorageAccountGetter.cs
+++ Azure.Storage.Connection.GetCloudStorageAccount/CloudStorageAccountGetter.cs
@@ -1,11 +1,11 @@
-using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
 
 namespace Azure.Storage.Connection.GetCloudStorageAccount
 {
     /// <summary>Function returns an instance of Microsoft Azure CloudStorageAccount using the given connection string</summary>
     public static class CloudStorageAccountGetter
     {
-        /// <summary>Returns an instance of Microsoft Azure ConfigurationManager using the given connection string</summary>
-        public static CloudStorageAccount ConfigurationManager(string connectionString) => CloudStorageAccount.Parse(connectionString);
+        /// <summary>Returns an instance of Microsoft Azure CloudStorageAccount using the given connection string</summary>
+        public static CloudStorageAccount StorageAccount(string connectionString) => CloudStorageAccount.Parse(connectionString);
     }
 }
